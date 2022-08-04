c1c1bbe2981e77cce35221f2ff9c00957ee67b48
Joe Maples <joe@maples.dev>
2022-08-04 11:57:45 -0400

    swlm: Move Event{Queue,Item} and custom dict to utils

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,59 @@ You should have received a copy of the GNU General Public License along with
 swlm. If not, see <https://www.gnu.org/licenses/>. 
 """
 
+from dataclasses import dataclass, field
 from optparse import OptionParser
+from dataclasses import dataclass, field
+import queue
+
+
+class WorkspaceLayoutManagerDict(dict):
+    def __missing__(self, key):
+        return None
+
+
+@dataclass
+class EventItem:
+    priority: int
+    event:  any
+
+    def __le__(self, b):
+         return self.priority <= b.priority
+
+    def __ge__(self, b):
+         return self.priority >= b.priority
+
+    def __lt__(self, b):
+         return self.priority < b.priority
+
+    def __gt__(self, b):
+         return self.priority > b.priority
+
+    def __eq__(self, b):
+         return self.priority == b.priority
+
+    def __ne__(self, b):
+         return self.priority != b.priority
+
+
+class EventQueue(queue.PriorityQueue):
+    def __init__(self, maxsize=0):
+        super().__init__(maxsize=0)
+        self.on_change_listeners = []
+
+
+    def _put(self, event):
+        # Add to queue
+        super()._put(event)
+
+        # Run any listeners
+        for listener in self.on_change_listeners:
+            thread = threading.Thread(target=listener)
+            thread.start()
+
+
+    def registerListener(self, listener):
+        self.on_change_listeners.append(listener)
 
 
 def getCommaSeparatedArgs(option, opt, value, parser):
