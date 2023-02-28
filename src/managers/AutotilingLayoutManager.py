472ed7b1f5ee78023a6b71181aed0b80a75dbe92
Joe Maples <joe@maples.dev>
2023-02-28 10:24:19 -0500

    Autotiling: Remove attempted optimization
    Sometimes this doesnt work, maybe window parentage is messed up?

--- src/managers/AutotilingLayoutManager.py
+++ src/managers/AutotilingLayoutManager.py
@@ -50,9 +50,6 @@ class AutotilingLayoutManager(WorkspaceLayoutManager):
         if window.parent.layout == "tabbed":
             return True
 
-        if window.parent.layout != window.layout and len(window.parent.nodes) <= 1:
-            return True
-
         return False
 
     def switchSplit(self, window):
